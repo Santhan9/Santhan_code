a396af1f7e4e12edb8f0e027f962d2dfc9b43c67
ADMIN <ADMIN@192.168.0.4>
2018-03-22 23:46:01 +0530

    hi

---
+++ Cucumber/src/testCucumber/testRunner.java
@@ -0,0 +1,15 @@
+package testCucumber;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature"
+		,glue={"stepDefenition"}
+		)
+
+public class testRunner {
+
+}
